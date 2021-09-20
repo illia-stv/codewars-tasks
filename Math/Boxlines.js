6fce7b59d5c0553d3da0de99decd3ef6c718499c
illya1074 <ilya@sheremetov.com>
2021-09-20 13:07:38 +0200

    next task

---
+++ Math/Boxlines.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/6129095b201d6b000e5a33f0/solutions/javascript
+
+const f = (x,y,z) => {
+  return (4+(y-1)*2+(x-1)*(2+(y-1)))*z+(4+3*(y-1)+(x-1)*(3+(y-1)*2))+(4+3*(y-1)+(x-1)*(3+(y-1)*2))*z 
+}
